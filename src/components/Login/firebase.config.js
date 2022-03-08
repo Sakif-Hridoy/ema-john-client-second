cca10f3e040e995173fd3e4f6a3bc041f7a999e7
Sakif <sakifworkspace@gmail.com>
2022-03-08 23:43:58 +0600

    add my firebase data to ema-john-auth template and and insert all fakedata into mongodb

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDWY-9jY2ws4707eW9UM4VD2-O7EEZFJHg",
+  authDomain: "ecom-fire-auth.firebaseapp.com",
+  projectId: "ecom-fire-auth",
+  storageBucket: "ecom-fire-auth.appspot.com",
+  messagingSenderId: "223725438947",
+  appId: "1:223725438947:web:5414e1abd5d577652502d6"
+};
+export default firebaseConfig;
