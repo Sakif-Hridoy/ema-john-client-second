15c8ed6319d8d3973826dfbd918c7f27e3c01aa4
Sakif <sakifworkspace@gmail.com>
2022-03-12 01:07:54 +0600

    add heroku url with replacing local hosts and add also firebase,then deploy app to firebase

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -8,7 +8,7 @@ const ProductDetail = () => {
     const [product,setProduct] = useState({})
     
     useEffect(()=>{
-        fetch('http://localhost:5000/product/' + productKey)
+        fetch('https://limitless-falls-44996.herokuapp.com/product/' + productKey)
         .then(res=>res.json())
         .then(data =>setProduct(data));
     },[productKey])
--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -12,7 +12,7 @@ const Shipment = () => {
     const savedCart = getDatabaseCart();
       const orderDetails = {...loggedInUser,products:savedCart,shipment:data,orderTime:new Date()};
 
-      fetch('http://localhost:5000/addOrder',{
+      fetch('https://limitless-falls-44996.herokuapp.com/addOrder',{
         method:'POST',
         headers: {
           'Content-Type':'application/json'
